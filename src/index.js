a37fa0900c6ccc689d2078f869c1feed007a8cd9
unknown <revchenkodzmitry@gmail.com>
2022-03-30 19:45:05 +0300

    feat: finish setup webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import "./style.css";
+import "./styles.scss";
+import "./assets/slides-1.jpg";
+import "./assets/slides-2.jpg";
+import "./assets/slides-3.jpeg";
+import "./assets/slides-4.jpeg";
+import "./assets/slides-5.jpg";
+import "./assets/slides-6.jpg";
+import "./assets/slides-7.jpg";
+import "./assets/slides-8.jpeg";
 
 console.log("привет");
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,56 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./styles.css",
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     {
+    //       from: "./src/assets",
+    //       to: "assets",
+    //     },
+    //   ],
+    // }),
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          publicPath: "/",
+          name: `assets/[name].[ext]`,
+        },
       },
     ],
   },
